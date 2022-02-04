b53ff8b8d5e9181e5edf6e67bbc90cd15766503a
jacobKessinger <jacobkessinger@live.com>
2022-02-04 16:36:49 -0500

    did the challenge

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -6,6 +6,7 @@ struct ExerciseView: View {
     let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
     let index: Int
     let interval: TimeInterval = 30
+    @State private var showAlert = false
     var body: some View {
         GeometryReader {
             geometry in
@@ -13,22 +14,41 @@ struct ExerciseView: View {
             {
                 HeaderView(exerciseName: exerciseNames[index])
                     .padding(.bottom)
+                
                 if let url = Bundle.main.url( forResource: videoNames[index], withExtension: "mp4") {
                     VideoPlayer(player: AVPlayer(url: url)).frame(height: geometry.size.height * 0.45)
                 } else {
                     Text("Couldn't find \(videoNames[index]).mp4").foregroundColor(.red)
                 }
-                Text(Date().addingTimeInterval(interval), style: .timer).font(.system(size:90))
+                
+                Text(Date()
+                        .addingTimeInterval(interval), style: .timer)
+                    .font(.system(size:90))
+                    .background(Rectangle().foregroundColor(.black).cornerRadius(15)
+                    )
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    
+                
                 Button("Start/Done")
                 {
                     
-                }.font(.title3).padding()
-                RatingView().padding()
+                }
+                    .font(.title3)
+                    .padding()
+                
+                RatingView()
+                    .padding()
+                
                 Spacer()
                 Button("History")
                 {
-                    
-                }.padding(.bottom)
+                    showAlert = true
+                }
+                    .alert(isPresented: $showAlert)
+                {
+                    Alert(title: Text("There is no history to show."))
+                }
+                    .padding(.bottom)
         }
         }
     }
--- HIITFit/Views/RatingView.swift
+++ HIITFit/Views/RatingView.swift
@@ -5,6 +5,7 @@ struct RatingView: View {
         HStack {
             ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { _ in
                 Image(systemName: "waveform.path.ecg").foregroundColor(.gray).font(.largeTitle)
+                    .background(Circle().foregroundColor(Color(red: 1, green: 0.349, blue: 0.349)))
             }
         }
     }
